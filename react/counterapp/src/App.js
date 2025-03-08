21d8a9d01a2f95d523e9fb3310d2eb47e0208946
shivangi-mis <shivangisu804@gmail.com>
2025-03-09 03:59:48 +0530

    Add files via upload

---
+++ react/counterapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>React Counter App</h1>
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
